fe95b3bc5c9c60387f72dd3420337d29328a76ff
VukasinVujic <vukasinvujic021@gmail.com>
2019-03-27 18:55:08 +0100

    getters 1

---
+++ src/components/counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>the safety stored number: {{ safelyStoredNumber }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    }
+  },
+  computed: {
+    ...mapGetters(["safelyStoredNumber"])
+  }
+};
+</script>
+
+
+
